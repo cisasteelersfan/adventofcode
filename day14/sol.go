466e7dedede2ed0ad0abf4b55d4dbf427af98a66
Colby Rome <cisasteelersfan@gmail.com>
2022-12-15 20:00:51 -0500

    Day 14 part 2.

--- day14/sol.go
+++ day14/sol.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	dat, _ := os.ReadFile("day14/input.txt")
 	m := make(map[point]bool)
+	largestY := 0
 	for _, line := range strings.Split(string(dat), "\n") {
 		points := strings.Split(line, " -> ")
 		for i := 1; i < len(points); i++ {
@@ -17,14 +18,49 @@ func main() {
 			dest := parsePoint(points[i])
 			for _, point := range getPoints(source, dest) {
 				m[point] = true
+				if point.y > largestY {
+					largestY = point.y
+				}
 			}
 		}
+	}
+	copy := make(map[point]bool)
+	for k, v := range m {
+		copy[k] = v
 	}
 	drops := 0
 	for i := 0; simulateDrop(500, -1000, m); i++ {
 		drops++
 	}
 	fmt.Println("part 1:", drops)
+
+	drops = 0
+	for i := 0; !copy[point{500, 0}]; i++ {
+		drops++
+		simulateDropWithFloor(500, 0, copy, largestY)
+	}
+	fmt.Println("part 2:", drops)
+}
+
+func simulateDropWithFloor(x, y int, m map[point]bool, largestY int) {
+	if y == largestY+1 {
+		m[point{x, y}] = true
+		return
+	}
+	if m[point{x, y + 1}] { // if below is occupied
+		if !m[point{x - 1, y + 1}] {
+			simulateDropWithFloor(x-1, y+1, m, largestY)
+			return
+		}
+		if !m[point{x + 1, y + 1}] {
+			simulateDropWithFloor(x+1, y+1, m, largestY)
+			return
+		}
+		m[point{x, y}] = true
+		return
+	}
+	simulateDropWithFloor(x, y+1, m, largestY)
+	return
 }
 
 // returns true if sand was placed
