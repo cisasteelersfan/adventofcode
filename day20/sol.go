49280a2441d5ea27692ed55f673baae8080d8987
Colby Rome <cisasteelersfan@gmail.com>
2022-12-22 20:27:41 -0500

    Day 20 part 2.

--- day20/sol.go
+++ day20/sol.go
@@ -25,10 +25,26 @@ func main() {
 	for _, n := range d.original {
 		d.advance(n)
 	}
-	fmt.Println("part 1:", d.part1())
+	fmt.Println("part 1:", d.sum())
+
+	for i := 1; i < len(d.original); i++ {
+		d.original[i-1].next = d.original[i]
+		d.original[i].prev = d.original[i-1]
+	}
+	d.original[0].prev = d.original[len(d.original)-1]
+	d.original[len(d.original)-1].next = d.original[0]
+	for _, n := range d.original {
+		n.num *= 811589153
+	}
+	for i := 0; i < 10; i++ {
+		for _, n := range d.original {
+			d.advance(n)
+		}
+	}
+	fmt.Println("part 2:", d.sum())
 }
 
-func (d *dll) part1() int {
+func (d *dll) sum() int {
 	zero := &node{}
 	for _, n := range d.original {
 		if n.num == 0 {
@@ -63,6 +79,7 @@ func (d *dll) print() {
 func (d *dll) advance(n *node) {
 	num := n.num
 	if num > 0 {
+		num = num % (len(d.original) - 1)
 		n.prev.next = n.next
 		n.next.prev = n.prev
 		last := n.prev
@@ -74,6 +91,7 @@ func (d *dll) advance(n *node) {
 		last.next = n
 		n.prev = last
 	} else if num < 0 {
+		num = num % (len(d.original) - 1)
 		n.prev.next = n.next
 		n.next.prev = n.prev
 		last := n.next
