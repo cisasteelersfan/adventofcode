ee06ca208be13eab320359dbdd912eca1e165eda
Colby Rome <cisasteelersfan@gmail.com>
2025-12-01 20:26:00 -0500

    Update newday to 2025.

--- newday.sh
+++ newday.sh
@@ -2,15 +2,15 @@
 
 : ${1?"Usage: $0 day"}
 
-mkdir -p "2024/day$1"
-touch "2024/day$1/sol.go"
-touch "2024/day$1/small.txt"
-touch "2024/day$1/input.txt"
+mkdir -p "2025/day$1"
+touch "2025/day$1/sol.go"
+touch "2025/day$1/small.txt"
+touch "2025/day$1/input.txt"
 
 echo """package main
 
 import \"os\"
 
 func main() {
-	dat, _ := os.ReadFile(\"2024/day$1/small.txt\")
-}""" >> "2024/day$1/sol.go"
+	dat, _ := os.ReadFile(\"2025/day$1/small.txt\")
+}""" >> "2025/day$1/sol.go"
