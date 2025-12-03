8d992aa3fe9332df367cb05c6739d94ef4b2b626
Colby Rome <cisasteelersfan@gmail.com>
2025-12-02 19:10:36 -0500

    Part 2.

--- 2025/day2/sol.go
+++ 2025/day2/sol.go
@@ -33,6 +33,37 @@ func main() {
 		}
 	}
 	fmt.Println("Part 1:", ans)
+
+	ans = 0
+	for _, r := range ranges {
+		for i := r.Start; i <= r.End; i++ {
+			if isInvalid(i) {
+				ans += i
+			}
+		}
+	}
+	fmt.Println("Part 2:", ans)
+}
+
+func isInvalid(i int) bool {
+	s := strconv.Itoa(i)
+	for size := 1; size < len(s); size++ {
+		sizeInvalid := true
+		if len(s)%size != 0 {
+			continue
+		}
+		first := s[0:size]
+		for j := 1; j < len(s)/size; j++ {
+			if first != s[j*size:j*size+size] {
+				// this SIZE is NOT invalid, but other sizes might be.
+				sizeInvalid = false
+			}
+		}
+		if sizeInvalid {
+			return true
+		}
+	}
+	return false
 }
 
 type Range struct {
