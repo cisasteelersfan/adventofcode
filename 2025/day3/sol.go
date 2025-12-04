7ec22f995677c4810d6b87978f7ef1dd8b94d735
Colby Rome <cisasteelersfan@gmail.com>
2025-12-03 21:55:21 -0500

    Part 1!

--- 2025/day3/sol.go
+++ 2025/day3/sol.go
@@ -12,18 +12,27 @@ func main() {
 	lines := strings.Split(string(dat), "\n")
 
 	ans := 0
+	m := make(map[string]int)
 	for _, line := range lines {
-		ans += findLargest(line)
+		ans += findLargest(line, m)
 	}
 	fmt.Println("Part 1:", ans)
 }
 
-func findLargest(line string) int {
+func findLargest(line string, m map[string]int) int {
+	if m[line] != 0 {
+		return m[line]
+	}
 	if len(line) == 2 {
+		m[line] = getNum(line)
 		return getNum(line)
 	}
-	first := findLargest(string(line[0]) + line[2:])
-	second := findLargest(line[1:])
+	f := string(line[0]) + line[2:]
+	first := findLargest(f, m)
+	m[f] = first
+
+	second := findLargest(line[1:], m)
+	m[line[1:]] = second
 	third := getNum(line[0:2])
 	return max(max(first, second), third)
 }
